a1edf405af6a8a0aa3a6beb8564856031a758fd5
Marta Ranzini <marta.ranzini.15@ucl.ac.uk>
2020-11-19 20:20:03 +0000

    added source code for fetal brain segmentation

---
+++ monaifbs/__about__.py
@@ -0,0 +1,6 @@
+__author__ = "Marta B.M. Ranzini"
+__email__ = "martabm.ranzini@gmail.com"
+__license__ = "Apache2.0"
+__version__ = "0.1.0"
+__summary__ = "MonaiFBS is a research-focused toolkit for " \
+    "fetal brain segmentation in 2D ultra-fast MRI."
